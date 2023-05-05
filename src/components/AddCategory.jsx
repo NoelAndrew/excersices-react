b1ea0111c942eef3ed2093988178e51be09b9e23
NoelAndrew <andrew98noel@gmail.com>
2023-05-05 12:47:00 -0600

    feature/ejercicio de estudio

---
+++ src/components/AddCategory.jsx
@@ -0,0 +1,26 @@
+import { useState } from "react"
+
+export const AddCategory = ( {onNewCategory} ) => {
+    const [inputValue, setInputValue] = useState( "" )
+    const onInputChange = (event) => {
+        setInputValue(event.target.value)
+    }
+    const onSubmmit = (event) => {
+        event.preventDefault();
+        if (inputValue.trim().length <= 1)  return;
+
+        onNewCategory( inputValue.trim() );
+        setInputValue("")
+    }
+
+    return (
+        <form onSubmit={ onSubmmit }>
+            <input 
+                type="text"
+                placeholder="Buscar Gifs"
+                value={inputValue}
+                onChange={(event) => onInputChange(event) }
+            />
+        </form>
+    )
+}
